ebdcfb00b36096e684beccfd77a0c1c6165bdee7
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2019-02-08 20:32:26 -0300

    Add files via upload

---
+++ mygl.h
@@ -0,0 +1,18 @@
+#ifndef _MYGL_H_
+#define _MYGL_H_
+
+#include "definitions.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
+//*****************************************************************************
+// Defina aqui as suas funções gráficas
+//*****************************************************************************
+
+
+
+
+
+#endif // _MYGL_H_
+
