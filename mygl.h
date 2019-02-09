f5b80b8a18c7c01276ebfd2dfb2eba395a03d690
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2019-02-09 19:04:55 -0300

    add putPixel()

--- mygl.h
+++ mygl.h
@@ -1,18 +1,20 @@
-#ifndef _MYGL_H_
+##ifndef _MYGL_H_
 #define _MYGL_H_
 
 #include "definitions.h"
+#include "math.h"
 #include <vector>
-#include <iostream>
-#include <string>
-#include <cmath>
+
 //*****************************************************************************
 // Defina aqui as suas funções gráficas
 //*****************************************************************************
 
-
-
+void putPixel(int x, int y, int cor[]){
+    
+    for(int i = 0; i < 4; i++){
+        FBptr[x*4+i + y*4*IMAGE_WIDTH] = cor[i];
+    }
+}
 
 
 #endif // _MYGL_H_
-
