687452985589dd12cc82dc176b5e289775ae4bc3
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2019-02-10 19:40:18 -0200

    Update mygl.h

--- mygl.h
+++ mygl.h
@@ -10,51 +10,85 @@
 //*****************************************************************************
 
 void putPixel(int x, int y, int cor[]){
-    
+
     for(int i = 0; i < 4; i++){
         FBptr[x*4+i + y*4*IMAGE_WIDTH] = cor[i];
     }
 }
 
 void midPointLine(int xi, int xf, int yi, int yf, int cor[]){
-    
+
     // 0 <= m <= 1
-    int dx = xf - xi;
-    int dy = yf - yi;
+    float dx = xf - xi;
+    float dy = yf - yi;
     float m = dy/dx;
     int d = 2 * dy - dx;
     int incr_e = 2 * dy;
     int incr_ne = 2 * (dy - dx);
     putPixel(xi, yi, cor);
-    if(0 < m <= 1.0){
-        while (xi < xf) {
-            if (d <= 0) {
-                d += incr_e;
-                xi++;
-            } else {
-                d += incr_ne;
-                xi++;
-                yi++;
+    if(0 < m && m <= 1.0){
+        if(dx > dy){
+            while (xi < xf) {
+                if (d <= 0) {
+                    d += incr_e;
+                    xi++;
+                } else {
+                    d += incr_ne;
+                    xi++;
+                    yi++;
+                }
+                putPixel(xi, yi, cor);
             }
-            putPixel(xi, yi, cor);
-        }   
+        }
+        else{
+            d = dy - 2*dx;
+            incr_e = 2*(dy - dx);
+            incr_ne = -2 * dy;
+            while(yi < yf){
+                if(d < 0){
+                    d += incr_e;
+                    yi++;
+                    xi++;
+                } else{
+                    d += incr_ne;
+                    yi++;
+                }
+                putPixel(xi, yi, cor);
+            }
+        }
     }
     else if(m > 1.0){
-        /*d = dy - 2*dx;
-        incr_e = 2*(dy- dx);
-        incr_ne = -2 * dy;*/
-        while(yi < yf){
-            if(d < 0){
-                d += incr_e;
-                yi++;
-                xi++;
-            } else{
-                d += incr_ne;
-                yi++;
+        if(dx > dy){
+            while (xi < xf) {
+                if (d <= 0) {
+                    d += incr_e;
+                    xi++;
+                } else {
+                    d += incr_ne;
+                    xi++;
+                    yi++;
+                }
+                putPixel(xi, yi, cor);
+            }
+        }
+        else{
+            d = dy - 2*dx;
+            incr_e = 2*(dy - dx);
+            incr_ne = -2 * dy;
+            while(yi < yf){
+                if(d < 0){
+                    d += incr_e;
+                    yi++;
+                    xi++;
+                } else{
+                    d += incr_ne;
+                    yi++;
+                }
+                putPixel(xi, yi, cor);
             }
         }
     }
 }
 
-#endif // _MYGL_H_
 
+#endif // _MYGL_H_
