6e399d57bc890056b958b7311adda9f8b6587966
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2019-02-10 20:52:38 -0200

    add algoritmo de bresenham

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-##include <array>
+
 #include <iostream>
 #include "main.h"
 #include "mygl.h"
@@ -9,10 +9,11 @@ void MyGlDraw(void)
 	//*************************************************************************
 	// Chame aqui as funções do mygl.h
 	//**************************************************************************
-    int ar[4] = {255, 0, 0, 1};
+    int red[4] = {255, 0, 0, 1};
     int blue[4] = {0, 0, 255, 1};
-    
-    putPixel(40, 40, ar);
+    int green[4] = {0, 255, 0, 1};
+
+   /* putPixel(40, 40, ar);
     putPixel(45, 40, ar);
     putPixel(30, 40, ar);
     putPixel(20, 40, ar);
@@ -24,9 +25,9 @@ void MyGlDraw(void)
     putPixel(50,40, ar);
     putPixel(55,40, ar);
     putPixel(60,40, ar);
-    putPixel(65,40, ar);
-    
-   putPixel(5,30, blue);
+    putPixel(65,40, ar);*/
+
+   /*putPixel(5,30, blue);
    putPixel(10,30, blue);
    putPixel(15,30, blue);
    putPixel(20,30, blue);
@@ -38,7 +39,13 @@ void MyGlDraw(void)
    putPixel(50,30, blue);
    putPixel(55,30, blue);
    putPixel(60,30, blue);
-   putPixel(65,30, blue);
+   putPixel(65,30, blue);*/
+
+   //midPointLine(200, 400, 200, 500, red);
+   midPointLine(200,400,200,400, blue);
+   midPointLine(200,201,200,400, red);
+   midPointLine(200,0,200,201, green);
+   midPointLine(200,0,200,0, red);
 }
 
 //-----------------------------------------------------------------------------
@@ -50,7 +57,7 @@ int main(int argc, char **argv)
 	InitDataStructures();
 
 	// Ajusta a função que chama as funções do mygl.h
-	DrawFunc = MyGlDraw;	
+	DrawFunc = MyGlDraw;
 
 	// Framebuffer scan loop.
 	glutMainLoop();
