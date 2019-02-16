c53338d1165026a4485a9e4fb4043240c3fe5f0a
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2019-02-16 18:29:58 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ void MyGlDraw(void)
 
 	// Chame aqui as funções do mygl.h
 	//**************************************************************************
+
+
     int red[4] = {255, 0, 0, 1};
     int blue[4] = {0, 0, 255, 1};
     int green[4] = {0, 255, 0, 1};
@@ -15,38 +17,26 @@ void MyGlDraw(void)
     int pink[4] = {255, 90, 150, 1};
     int yellow[4] = {255, 200, 100, 1};
 
-    /*putPixel(45, 40, ar);
-    putPixel(30, 40, ar);
-    putPixel(20, 40, ar);
-    putPixel(35, 40, ar);
-    putPixel(25,40, ar);
-    putPixel(15,40, ar);
-    putPixel(10,40, ar);
-    putPixel(5,40, ar);
-    putPixel(50,40, ar);
-    putPixel(55,40, ar);
-    putPixel(60,40, ar);
-    putPixel(65,40, ar);*/
+    Posicao p1, p2, p3;
 
-   /*putPixel(5,30, blue);
-   putPixel(10,30, blue);
-   putPixel(15,30, blue);
-   putPixel(20,30, blue);
-   putPixel(25,30, blue);
-   putPixel(30,30, blue);
-   putPixel(35,30, blue);
-   putPixel(40,30, blue);
-   putPixel(45,30, blue);
-   putPixel(50,30, blue);
-   putPixel(55,30, blue);
-   putPixel(60,30, blue);
-   putPixel(65,30, blue);*/
+    p1.xi = 9;
+    p1.xf = 500;
+    p1.yi = 10;
+    p1.yf = 11;
+
+    p2.xi = 9;
+    p2.xf = 250;
+    p2.yi = 10;
+    p2.yf = 250;
+
+    p3.xi = 500;
+    p3.xf = 250;
+    p3.yi = 10;
+    p3.yf = 250;
 
    //drawLine(200,510,200,201, green);
    //drawLine(600, 1, 600, 0, blue);
-   drawLine(9,500,10,11, blue);
-   drawLine(9,250,10,250, blue);
-   drawLine(500,250,10,250, blue);
+   drawTriangles(p1, p2, p3, blue);
 }
 
 //-----------------------------------------------------------------------------
