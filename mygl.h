9587d469c36928ff503b140f11b7c13d75f7da5e
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2019-02-16 18:20:38 -0200

    Update mygl.h

--- mygl.h
+++ mygl.h
@@ -3,12 +3,19 @@
 
 #include "definitions.h"
 #include "math.h"
-#include <vector>
 #include <stdlib.h>
 
 //*****************************************************************************
 // Defina aqui as suas funções gráficas
 //*****************************************************************************
+
+struct Posicao{
+    int xi;
+    int xf;
+    int yi;
+    int yf;
+};
+
 
 void putPixel(int x, int y, int cor[]){
 
@@ -17,42 +24,16 @@ void putPixel(int x, int y, int cor[]){
     }
 }
 
-/*int *random(){
-    int a[3] = {0, 0, 0};
-        for(int i = 0; i < 3; i++){
-            int r = rand() % 255;
-            if(r >= 0 && r <= 255){
-                a[i] = r;
-            }
-        }
-
-    return a;
-}*/
 
 void interpolation(int i, int cor[]){
     cor[0] = i;
-    cor[1] = 150-i;
+    cor[1] = 255-i;
 }
-/*void interpolation(int cor[], int b[]){
-    float t = 0.5;
-    for(int i = 0; i < 3; i++){
-        //gera números aleatórios entre 0 e 255
-            int r = rand() % 255;
-            if(r >= 0 && r <= 255){
-            //coloca os números dentro do array
-                b[i] = r;
-            }
-        }
-    //int *p = random();
-    cor[0] = cor[0] + (b[0] - cor[0]) * t;
-    cor[1] = cor[1] + (b[1] - cor[1]) * t;
-    cor[2] = cor[2] + (b[2] - cor[2]) * t;
-}*/
+
 
 void drawLine(int xi, int xf, int yi, int yf, int cor[]){
 
     // 0 <= m <= 1
-    int a[4] = {200,100,200,1};
     float dx = xf - xi;
     float dy = yf - yi;
     float m = dy/dx;
@@ -61,7 +42,7 @@ void drawLine(int xi, int xf, int yi, int yf, int cor[]){
     int incr_e = 2 * dy;
     int incr_ne = 2 * (dy - dx);
     putPixel(xi, yi, cor);
-    if((m > 0 && m <= 1.0) || m > 1.0){
+    if((m >= 0 && m <= 1.0) || m > 1.0){
         if(dx > dy){
             while (xi < xf) {
                 if (d <= 0) {
@@ -89,7 +70,7 @@ void drawLine(int xi, int xf, int yi, int yf, int cor[]){
         else{
             d = dy - 2*dx;
             incr_e = 2*(dy - dx);
-            incr_ne = -2 * dy;
+            incr_ne = -2 * dx;
             while(yi < yf){
                 if(d < 0){
                     d += incr_e;
@@ -129,7 +110,6 @@ void drawLine(int xi, int xf, int yi, int yf, int cor[]){
                     xf++;
                 }
                 interpolation(i, cor);
-                //interpolation(cor, a);
                 putPixel(xf, yf, cor);
                 i++;
             }
@@ -157,7 +137,6 @@ void drawLine(int xi, int xf, int yi, int yf, int cor[]){
                     xf++;
                 }
                 interpolation(i,cor);
-                //interpolation(cor, a);
                 putPixel(xi, yi, cor);
                 i++;
             }
@@ -167,4 +146,3 @@ void drawLine(int xi, int xf, int yi, int yf, int cor[]){
 
 
 #endif // _MYGL_H_
-
